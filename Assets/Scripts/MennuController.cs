1431cafe13c2f81083b8c5d4f87e66d9b3498d02
TranMinhTamttttt <tamtran11037469@gmail.com>
2025-10-14 10:06:28 +0700

    Start Button

---
+++ Assets/Scripts/MennuController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MennuController : MonoBehaviour
+{
+    public void OnStartButton()
+    {
+        int lastLevel = PlayerPrefs.GetInt("LastLevel", 1); 
+        Debug.Log("Loading Level: Level" + lastLevel); 
+        SceneManager.LoadScene("Level" + lastLevel);
+    }
+}
